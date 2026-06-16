4f549d81e6c82504effbea9d7042ed9769b68064
abdul786-student <64031144+abdul786-student@users.noreply.github.com>
2026-06-16 20:21:18 +0530

    Filter page updated

--- pageObjects-tricentics/addComputersToCartPage.js
+++ pageObjects-tricentics/addComputersToCartPage.js
@@ -7,6 +7,12 @@ class addComputersToCartPage{
         this.quantity=page.locator("#addtocart_72_EnteredQuantity");
         this.AddToCart=page.locator('#add-to-cart-button-72');
         this.content=page.locator(".content");
+        this.productUnitPrice=page.locator('.product-unit-price');
+        this.productSelected=page.locator(".product-name");
+        this.quantity=page.locator('.qty-input');
+        this.cart=page.locator("a[class='ico-cart'] span[class='cart-label']");
+        this.productSubTotal=page.locator(".product-subtotal");
+        //this.prices=null;
 
     }
 
@@ -26,6 +32,23 @@ class addComputersToCartPage{
         await this.quantity.fill("2");
         await this.AddToCart.click();
         await expect(this.content).toBeVisible();
+
+    }
+
+    async fillCartDetails(){
+        
+        let countQuantity;
+        let ExpectedtotalAmount;
+        let ActualtotalAmount;
+        await this.cart.click();
+        await this.cart.first().waitFor();
+        await expect(this.productSelected).toBeVisible();
+        await this.productSelected.waitFor();
+        countQuantity=await this.quantity.getAttribute('value');
+        let prices=await this.productUnitPrice.textContent();
+        ExpectedtotalAmount=countQuantity * prices;
+        ActualtotalAmount=Number(await this.productSubTotal.textContent());
+        await expect(ExpectedtotalAmount).toBe(ActualtotalAmount);
 
     }
     
--- tests/Tricenticsfilter.spec.js
+++ tests/Tricenticsfilter.spec.js
@@ -17,6 +17,19 @@ test.describe('Tricentics Ecom App Login', () => {
         const ram=testData.ComputerConfig.ram;
         const hdd=testData.ComputerConfig.hdd;
         const softwares=testData.ComputerConfig.softwares;
+        const countryName = testData.shippingDetails.countryName;
+        const pinCode = testData.shippingDetails.pinCode;
+        const city = testData.shippingDetails.city;
+        const address1 = testData.shippingDetails.address1;
+        const phoneNumber = testData.shippingDetails.phoneNumber;
+        const ExpectedShippingMethod = testData.orderDetails.ExpectedShippingMethod;
+        const PaymentMethod = testData.orderDetails.PaymentMethod;
+        const cardType = testData.cardDetails.cardType;
+        const cardholderName = testData.cardDetails.cardholderName;
+        const cardNumber = testData.cardDetails.cardNumber;
+        const expireMonth = testData.cardDetails.expireMonth;
+        const expireYear = testData.cardDetails.expireYear;
+        const cardCode = testData.cardDetails.cardCode;
 
         const poManager=new POManager(page);
         const loginPage=poManager.getLoginPage();
@@ -33,6 +46,18 @@ test.describe('Tricentics Ecom App Login', () => {
         const addcomputerpage=poManager.getComputerdetails();
         await addcomputerpage.addConfigDetails(processor,ram,hdd,softwares);
         await addcomputerpage.addProductToCart();
+        await addcomputerpage.fillCartDetails();
+
+        const cartpage=poManager.getcartPage();
+        await cartpage.getCountry(countryName);
+        await cartpage.fillPostalCode(pinCode);
+
+        const checkoutpage=poManager.getcheckOutPage();
+        await checkoutpage.checkOutProduct(userName,countryName,city,address1,pinCode,phoneNumber);
+        await checkoutpage.getShippingMethod(ExpectedShippingMethod);
+        await checkoutpage.getPaymentMethod(PaymentMethod);
+        await checkoutpage.getCardDetails(cardType, cardholderName, cardNumber, expireMonth, expireYear, cardCode);
+        await checkoutpage.confirmOrderr();
 
 
         
